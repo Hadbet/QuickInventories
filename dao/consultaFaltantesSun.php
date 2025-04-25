adbc415430f0b3f02b03afe0acc434508056cf46
hadbet <hadbet.altamirano@grammer.com>
2025-04-25 14:32:28 -0600

    Inicio de historicos de conteo

--- dao/consultaFaltantesSun.php
+++ dao/consultaFaltantesSun.php
@@ -1,23 +1,30 @@
 <?php
 include_once('db/db_Inventario.php');
 
-ContadorApu();
+header('Content-Type: application/json'); // Asegura que la respuesta sea JSON
 
-function ContadorApu() {
+try {
     $con = new LocalConector();
     $conex = $con->conectar();
 
-    $datos = mysqli_query($conex, "SELECT 
-        i.`STLocation`, 
-        i.`STBin`, 
-        i.`STType`, 
-        i.`GrammerNo`, 
-        i.`Cantidad`, 
-        i.`AreaCve`,
-        CONCAT('<button class=\"btn btn-info text-white\" onclick=\"llenarDatos(\\\'', 
-               i.`GrammerNo`, '\\\', \\\'', 
-               i.`STBin`, '\\\', \\\'', 
-               i.`STType`, '\\\')\">Modificar</button>') AS `Boton`
+    if (!$conex) {
+        throw new Exception("Error de conexión a la base de datos");
+    }
+
+    $query = "
+        SELECT 
+            i.`STLocation`, 
+            i.`STBin`, 
+            i.`STType`, 
+            i.`GrammerNo`, 
+            i.`Cantidad`, 
+            i.`AreaCve`,
+            CONCAT(
+                '<button class=\"btn btn-info text-white\" onclick=\"llenarDatos(\'', 
+                i.`GrammerNo`, '\', \'', 
+                i.`STBin`, '\', \'', 
+                i.`STType`, '\')\">Modificar</button>'
+            ) AS `Boton`
         FROM `InventarioSap` i
         WHERE NOT EXISTS (
             SELECT 1 
@@ -26,9 +33,20 @@ function ContadorApu() {
               AND s.`Storage_Bin` = i.`STBin`
               AND s.`Storage_Type` = i.`STType`
               AND s.`Estatus` = 1
-        )");
+        )
+    ";
 
-    $resultado = mysqli_fetch_all($datos, MYSQLI_ASSOC);
-    echo json_encode(array("data" => $resultado));
+    $result = mysqli_query($conex, $query);
+
+    if (!$result) {
+        throw new Exception("Error en la consulta SQL: " . mysqli_error($conex));
+    }
+
+    $data = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    echo json_encode(["data" => $data]);
+
+} catch (Exception $e) {
+    http_response_code(500);
+    echo json_encode(["error" => $e->getMessage()]);
 }
 ?>
